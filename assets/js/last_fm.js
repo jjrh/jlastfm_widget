71bb33fe9ffee0df18d680b5a26a14f461035030
Justin Hornosty <justin@credil.org>
2013-11-27 10:09:50 -0500

    refactored js to be in a class style structure. added some comments to html

--- assets/js/last_fm.js
+++ assets/js/last_fm.js
@@ -1,56 +1,74 @@
+/* jlastfm 
+   couple notes;
+    so there is a issue where a function is called (mostly when nested in a promise) where we lose the context of 'this'
+    I have created var parentThis to deal with that fact and passed it in. This seems like a total hack and not the proper
+    way to do things. If anyone ever sees this code and knows the correct way of doing this please give me a shout.
 
-var rss_urls = {
-    "last_10_songs": "http://ws.audioscrobbler.com/1.0/user/slicenice/recenttracks.rss",
-    "loved_tracks": "http://ws.audioscrobbler.com/2.0/user/slicenice/lovedtracks.rss",
+    I understand jquery has a way of doing things better (and perhaps this should be a jquery plugin?)
     
-}
-var xml_urls = {
+        -jjrh (Nov 2013).
+ */
 
-    "artists": {
-	"weeklyartistchart": "http://ws.audioscrobbler.com/2.0/user/slicenice/weeklyartistchart.xml",
-	"top_artists": "http://ws.audioscrobbler.com/2.0/user/slicenice/topartists.xml",
-	"top_artists_3month": "http://ws.audioscrobbler.com/2.0/user/slicenice/topartists.xml?period=3month",
-	"top_artists_6month": "http://ws.audioscrobbler.com/2.0/user/slicenice/topartists.xml?period=6month",
-	"top_artists_12month": "http://ws.audioscrobbler.com/2.0/user/slicenice/topartists.xml?period=12month",
-    },
-    "tracks": {
-	"weeklytrackchart": "http://ws.audioscrobbler.com/2.0/user/slicenice/weeklytrackchart.xml",
-	"top_tracks": "http://ws.audioscrobbler.com/2.0/user/slicenice/toptracks.xml",
-	"top_tracks_3month": "http://ws.audioscrobbler.com/2.0/user/slicenice/toptracks.xml?period=3month",
-	"top_tracks_6month": "http://ws.audioscrobbler.com/2.0/user/slicenice/toptracks.xml?period=6month",
-	"top_tracks_12month": "http://ws.audioscrobbler.com/2.0/user/slicenice/toptracks.xml?period=12month",
+var jlastfm = function(username){
+
+    this.rss_urls = {
+	"last_10_songs": "http://ws.audioscrobbler.com/1.0/user/"+username+"/recenttracks.rss",
+	"loved_tracks": "http://ws.audioscrobbler.com/2.0/user/"+username+"/lovedtracks.rss",
+	
     }
+    this.xml_urls = {
+
+	"artists": {
+	    "weeklyartistchart": "http://ws.audioscrobbler.com/2.0/user/"+username+"/weeklyartistchart.xml",
+	    "top_artists": "http://ws.audioscrobbler.com/2.0/user/"+username+"/topartists.xml",
+	    "top_artists_3month": "http://ws.audioscrobbler.com/2.0/user/"+username+"/topartists.xml?period=3month",
+	    "top_artists_6month": "http://ws.audioscrobbler.com/2.0/user/"+username+"/topartists.xml?period=6month",
+	    "top_artists_12month": "http://ws.audioscrobbler.com/2.0/user/"+username+"/topartists.xml?period=12month",
+	},
+	"tracks": {
+	    "weeklytrackchart": "http://ws.audioscrobbler.com/2.0/user/"+username+"/weeklytrackchart.xml",
+	    "top_tracks": "http://ws.audioscrobbler.com/2.0/user/"+username+"/toptracks.xml",
+	    "top_tracks_3month": "http://ws.audioscrobbler.com/2.0/user/"+username+"/toptracks.xml?period=3month",
+	    "top_tracks_6month": "http://ws.audioscrobbler.com/2.0/user/"+username+"/toptracks.xml?period=6month",
+	    "top_tracks_12month": "http://ws.audioscrobbler.com/2.0/user/"+username+"/toptracks.xml?period=12month",
+	}
+    }
+
+    this.stat_data = {"artists":{},"tracks":{},"weeklycharts":{} }    
 }
 
-var stat_data = {"artists":{},"tracks":{},"weeklycharts":{} }    
+jlastfm.prototype.init = function(){
+	this.xml_all();
+	this.get_last_10();
+}
 
-
-
-/* do RSS stuff here... */
-function get_last_10(){
-    var RSS_URL = rss_urls["last_10_songs"]
+    /* do RSS stuff here... */
+jlastfm.prototype.get_last_10 = function(){
+    var parentThis = this;
+    var RSS_URL = this.rss_urls["last_10_songs"]
     $.ajax({
 	url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(RSS_URL),
 	dataType: 'json',
 	success: function(data) {
-	    build_dom_rss(data.responseData.feed); //dump_data(data.responseData.feed,selector);
+	    parentThis.build_dom_rss(data.responseData.feed); //dump_data(data.responseData.feed,selector);
 	}
     });
 }
-//"Ryan Paris – Dolce Vita (Part I - Vocal) (Extended Disco Mix)"
-function build_dom_rss(data){
+
+    //"Ryan Paris – Dolce Vita (Part I - Vocal) (Extended Disco Mix)"
+jlastfm.prototype.build_dom_rss = function(data){
     $("#last_10_songs").append("<ul>");
     entries = data.entries;
     for(var i=0; i< entries.length; i++){
 	/*
 	  entries[x] = {
-	      author: "",
-	      categories: Array[0],
-	      content: "http://www.last.fm/music/Accidental+Heroes",
-	      contentSnippet: "http://www.last.fm/music/Accidental+Heroes",
-	      link: "http://www.last.fm/user/slicenice#1385069306",
-	      publishedDate: "Thu, 21 Nov 2013 13:28:26 -0800",
-	      title: "Accidental Heroes – Precinct 13"
+	  author: "",
+	  categories: Array[0],
+	  content: "http://www.last.fm/music/Accidental+Heroes",
+	  contentSnippet: "http://www.last.fm/music/Accidental+Heroes",
+	  link: "http://www.last.fm/user/slicenice#1385069306",
+	  publishedDate: "Thu, 21 Nov 2013 13:28:26 -0800",
+	  title: "Accidental Heroes – Precinct 13"
 	  }
 	  Taking this example we can see content does point to the artists page. Still, content seems questionable as to 
 	  what it implies. Current method of hacking up the song title seems appropiate.
@@ -74,7 +92,7 @@ function build_dom_rss(data){
 	var rank = "";
 
 	/* trying to get /when/ it was played.
-	   * we run into some issues with spacing
+	 * we run into some issues with spacing
 	 */
 	var publish_date = new Date(entries[i].publishedDate);
 	var diff = new Date() - publish_date;
@@ -86,63 +104,61 @@ function build_dom_rss(data){
 	    format_string = publish_date.toString();
 	    format_string = format_string.split(" ");
 	    format_string = format_string[1]+" "+format_string[2]+","+format_string[4].split(":")[0]+":"+format_string[4].split(":")[1];
-	    console.log(format_string);
+//	    console.log(format_string);
 	    plays = format_string
 	}
 	else{
-	    console.log(diff + " hours ago");
+//	    console.log(diff + " hours ago");
 	    plays = diff + "h";
 	}
 	
 
 	
-	var html_entry = make_entry_template(artist,track,rank,plays);
+	var html_entry = this.make_entry_template(artist,track,rank,plays);
 	$("#last_10_songs").append(html_entry);
     }
     $("#last_10_songs").append("</ul>");
     
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function xml2json(url,type,period){
+jlastfm.prototype.xml2json = function(url,type,period){
     var def = $.Deferred();
+    var t = this;
     $.get(url, function(xml){
-	stat_data[type][period] = $.xml2json(xml);
+	t.stat_data[type][period] = $.xml2json(xml);
 	def.resolve();
     });
     return def.promise();
 }
-			    
 
-var xml_all = function(){
+
+jlastfm.prototype.xml_all = function(){
     /* I don't understand why I can't pass in a array of promises */
-    $.when(xml2json(xml_urls["tracks"]["weeklytrackchart"],"weeklycharts","weeklytrackchart"),
-	   xml2json(xml_urls["tracks"]["top_tracks"],"tracks","top_tracks"), 
-	   xml2json(xml_urls["tracks"]["top_tracks_3month"],"tracks","top_tracks_3month" ),
-	   xml2json(xml_urls["tracks"]["top_tracks_6month"],"tracks","top_tracks_6month"),
-	   xml2json(xml_urls["tracks"]["top_tracks_12month"],"tracks","top_tracks_12month"),
-	   xml2json(xml_urls["artists"]["weeklyartistchart"],"weeklycharts","weeklyartistchart"),
-	   xml2json(xml_urls["artists"]["top_artists"],"artists","top_artists"), 
-	   xml2json(xml_urls["artists"]["top_artists_3month"],"artists","top_artists_3month"),
-	   xml2json(xml_urls["artists"]["top_artists_6month"],"artists","top_artists_6month"),
-	   xml2json(xml_urls["artists"]["top_artists_12month"],"artists","top_artists_12month")
-	  ).done(build_dom_xml);
+    var parentThis = this;
+    $.when(this.xml2json(this.xml_urls["tracks"]["weeklytrackchart"],"weeklycharts","weeklytrackchart"),
+	   this.xml2json(this.xml_urls["tracks"]["top_tracks"],"tracks","top_tracks"), 
+	   this.xml2json(this.xml_urls["tracks"]["top_tracks_3month"],"tracks","top_tracks_3month" ),
+	   this.xml2json(this.xml_urls["tracks"]["top_tracks_6month"],"tracks","top_tracks_6month"),
+	   this.xml2json(this.xml_urls["tracks"]["top_tracks_12month"],"tracks","top_tracks_12month"),
+	   this.xml2json(this.xml_urls["artists"]["weeklyartistchart"],"weeklycharts","weeklyartistchart"),
+	   this.xml2json(this.xml_urls["artists"]["top_artists"],"artists","top_artists"), 
+	   this.xml2json(this.xml_urls["artists"]["top_artists_3month"],"artists","top_artists_3month"),
+	   this.xml2json(this.xml_urls["artists"]["top_artists_6month"],"artists","top_artists_6month"),
+	   this.xml2json(this.xml_urls ["artists"]["top_artists_12month"],"artists","top_artists_12month")
+	  ).done(function(d,t,x){
+	      // bit of a hack here, we need to pass in the context of this to the child elements
+	      parentThis.build_dom_xml(d,t,x,parentThis); 
+	  });
 }
 
-function build_dom_xml(stat_data_selector,id_selector){
-    /* generate tracks html stuff .... */
-    build_tracks();
-    build_artists();
-    build_charts();
-
-}
 
 /* data should look like:
 
-*/
+ */
 
-function make_entry_template(artist,track,rank,plays){
+jlastfm.prototype.make_entry_template = function(artist,track,rank,plays){
     html_dump = "<li>";
     html_dump = html_dump+"<span>";
     html_dump = html_dump+"<b>"+rank+". </b>"
@@ -162,8 +178,9 @@ function make_entry_template(artist,track,rank,plays){
     return html_dump
 }
 
-function build_tracks(){    
-    $.each(stat_data["tracks"], function(k,v){
+jlastfm.prototype.build_tracks = function(){    
+    var parentThis = this;
+    $.each(this.stat_data["tracks"], function(k,v){
 	var track_type = v["toptracks"]["@attributes"]["type"];
 	var tracks = v["toptracks"]["track"];
 
@@ -171,9 +188,7 @@ function build_tracks(){
 	for(var i = 0; i< tracks.length; i++){
 	    var artist = {"title":tracks[i].artist.name.value,
 			  "url": tracks[i].artist.url.value
-			  }
-
-
+			 }
 	    var track = {"title": tracks[i].name.value,
 			 "url":tracks[i].url.value
 			}
@@ -181,16 +196,17 @@ function build_tracks(){
 	    var rank = tracks[i]['@attributes'].rank;
 	    var plays = tracks[i].playcount.value;
 
-	    var html_entry = make_entry_template(artist,track,rank,plays);
+	    var html_entry = parentThis.make_entry_template(artist,track,rank,plays);
 	    $("#songs_"+track_type).append(html_entry)
 	}
 	$("#songs_"+track_type).append("</ul>");
     });
 }
 
-function build_artists(){
+jlastfm.prototype.build_artists = function(){
     /* generate artist html stuff .... */
-    $.each(stat_data["artists"], function(k,v){
+    var parentThis = this;
+    $.each(this.stat_data["artists"], function(k,v){
 	var artist_type = v["topartists"]["@attributes"]["type"];
 	var artists = v["topartists"]["artist"];
 	
@@ -206,7 +222,7 @@ function build_artists(){
 	    var plays = artists[i].playcount.value;
 	    var rank = artists[i]['@attributes'].rank;
 
-	    var html_entry = make_entry_template(artist,track,rank,plays);
+	    var html_entry = parentThis.make_entry_template(artist,track,rank,plays);
 	    $("#artists_"+artist_type).append(html_entry)
 	}
 	$("#artists_"+artist_type).append("</ul>");
@@ -217,8 +233,9 @@ function build_artists(){
 }
 
 
-function build_charts(){
-    $.each(stat_data["weeklycharts"]["weeklytrackchart"], function(k,v){
+jlastfm.prototype.build_charts = function(){
+    var parentThis = this;
+    $.each(this.stat_data["weeklycharts"]["weeklytrackchart"], function(k,v){
 	// var track_type = v["toptracks"]["@attributes"]["type"];
 	var track_type = "week";
 	var tracks = v["track"];
@@ -229,18 +246,18 @@ function build_charts(){
 			 }
 	    var track = {"title":tracks[i].name.value,
 			 "url":tracks[i].url.value
-			 }
+			}
 	    var plays = tracks[i].playcount.value;
 	    var rank = tracks[i]['@attributes'].rank;
 
-	    var html_entry = make_entry_template(artist,track,rank,plays);
+	    var html_entry = parentThis.make_entry_template(artist,track,rank,plays);
 	    $("#songs_"+track_type).append(html_entry)
 	}
 	$("#songs_"+track_type).append("</ul>");
     }); 
 
     /* generate weekly artists charts .... */
-    $.each(stat_data["weeklycharts"]["weeklyartistchart"], function(k,v){
+    $.each(this.stat_data["weeklycharts"]["weeklyartistchart"], function(k,v){
 	var artist_type = "week" //v["weeklyartistchart"]["@attributes"]["type"];
 	var artists = v["artist"];
 	
@@ -251,19 +268,27 @@ function build_charts(){
 			 }
 	    var track = {"title":"",
 			 "url":"",
-			 }
+			}
 
 	    var plays = artists[i].playcount.value;
 	    var rank = artists[i]['@attributes'].rank
 
-	    var html_entry = make_entry_template(artist,track,rank,plays);
+	    var html_entry = parentThis.make_entry_template(artist,track,rank,plays);
 	    $("#artists_"+artist_type).append(html_entry);
 	}
 	$("#artists_"+artist_type).append("</ul>");
     });
 }
 
- 
+jlastfm.prototype.build_dom_xml = function(t,d,x,parentThis){ //function(stat_data_selector,id_selector,t){
+    /* generate tracks html stuff .... */
+    parentThis.build_tracks();
+    parentThis.build_artists();
+    parentThis.build_charts();
+
+}
+
+
 
 /*
 <div class="col-md-4 small" id="lastfm">
